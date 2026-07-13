303c8c2a5c54d792a2acfcb0bd43dee635b3ec6c
linklouis <119512962+linklouis@users.noreply.github.com>
2026-07-12 19:02:56 -0500

    Built agent based on tutorial. Untested

--- backend/models/reinforcement-learning/training.py
+++ backend/models/reinforcement-learning/training.py
@@ -1,14 +1,18 @@
 from simulation import DynamicSlotTradingEnv
 from data_management import StockDataFeed
 
+from agent import Agent
+
 feed = StockDataFeed("master_feed.parquet")
 
 env = DynamicSlotTradingEnv(data_feed=feed, initial_balance=10000.0)
+
+agent = Agent(5, 51) # TODO: pick actual values
 
 done = False
 obs, _ = env.reset()
 
 while not done:
-    action = env.action_space.sample()  # Replace with agent's action
+    action = agent.choose_action(obs) #env.action_space.sample()  # Replace with agent's action
     obs, reward, done, info = env.step(action)
     print(f"Step: {env.current_step}, Reward: {reward:.2f}, Portfolio Value: {env.portfolio_value:.2f}")
